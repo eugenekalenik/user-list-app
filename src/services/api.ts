3fab8072fc11b254ff5888f0ce3d313f819ce5a9
Eugene Kalenik <eugene.kalenik@gmail.com>
2024-10-29 08:58:48 -0400

    fix api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,8 @@ import { User } from '../types';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
-const fetchUsers = async (page: number = 1, perPage = 6, delay = 1): Promise<User[]> => {
-  const response = await fetch(`${BASE_URL}/users?page=${page}&per_page=${perPage}delay=${delay}`);
+const fetchUsers = async (page: number = 1, perPage = 6, delay = 3): Promise<User[]> => {
+  const response = await fetch(`${BASE_URL}/users?page=${page}&per_page=${perPage}&delay=${delay}`);
   const { data } = await response.json();
   return data;
 };
